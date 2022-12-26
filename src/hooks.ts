7d25deedcd2a27208d3c59ded736e2fe91d50ff1
lily1215z <odessa1215@i.ua>
2022-12-26 15:38:10 +0100

    remove folder .idea

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,8 @@
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {AppDispatch, AppRootState} from "./redux/store";
 
-
 // export const useAppDispatch = () => useDispatch<AppDispatch>()
 // export const useAppSelector: TypedUseSelectorHook<AppRootState>=useSelector
-
 
 //for Toolkit
 export const useAppDispatch: () => AppDispatch = useDispatch
